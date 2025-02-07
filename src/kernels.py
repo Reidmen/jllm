590ad89431dc0b9a352f3c2db91b39626a2ff0ca
Reidmen <r.rethmawn@gmail.com>
2025-02-07 17:43:57 +0100

    adding untracked kernel files

---
+++ src/kernels.py
@@ -0,0 +1,263 @@
+from jax.experimental import pallas as pl
+import jax
+from functools import partial
+import jax.numpy as jnp
+from jax import lax
+import numpy as np
+
+
+def get_mha_cost_estimate(shape_dtypes: list[jax.ShapeDtypeStruct]):
+    """
+    Get cost estimate for MHA on static shape information.
+
+    Args:
+        shape_dtypes:
+         - jax.ShapeDtypeStruct [batch_size, 1, num_heads, head_dim]
+         - jax.ShapeDtypeStruct [batch_size, seq_len, num_heads, head_dim]
+    Returns:
+        cost_estimate: float
+    """
+    batch_size, _, num_heads, head_dim = shape_dtypes[0].shape
+    seq_len = shape_dtypes[1].shape[1]
+
+    # Approximate computation of attention (as dot product softmax(QK^T) @ V)
+    flops = (
+        batch_size
+        * num_heads
+        * seq_len
+        * (
+            2 * head_dim  # QK^T
+            + head_dim  # softmax(QK^T)
+            + 2 * head_dim  # V
+        )
+    )
+
+    return pl.CostEstimate(
+        flops=flops,
+        transcendentals=batch_size * num_heads * seq_len,
+        bytes_accessed=sum(np.prod(shape) * dtype.itemsize for shape, dtype in shape_dtypes),
+    )
+
+
+@partial(jax.jit, static_argnums=["mask_value"])
+def reference_multiquery_attention(xq, xk, xv, lengths, *, mask_value: float = -float("inf")):
+    """
+    Reference implementation of multiquery attention.
+
+    Args:
+        xq: Query jax.Array [batch_size, num_heads, head_dim]
+        xk: Key jax.Array [batch_size, seq_len, head_dim]
+        xv: Value jax.Array [batch_size, seq_len, head_dim]
+        lengths: jax.Array i32 [batch_size]
+        mask_value: Value used for padding the attention. Default is -inf.
+
+    Returns:
+        attention_output: jax.Array [batch_size, num_heads, head_dim]
+        max_logit: jax.Array [batch_size, num_heads, 1]
+        softmax_denominator: jax.Array [batch_size, num_heads, 1]
+    """
+
+    # Compute logits and create attention mask
+    # [batch_size, num_heads, seq_len]
+    logits = jnp.einsum("bhd,bld->bhl", xq.astype(jnp.float32), xk.astype(jnp.float32))
+    # [1, seq_len] < [batch_size, 1] -> [batch_size, seq_len]
+    mask = jnp.arange(xk.shape[1])[None, :] < lengths[:, None]
+
+    # Apply mask and compute max logits
+    logits = logits + jnp.where(mask, 0.0, mask_value)[:, None]
+    # [batch_size, num_heads, seq_len] -> [batch_size, num_heads]
+    max_logits = logits.max(axis=-1)
+
+    # Compute unnormalized attention and denominator
+    unnormalized_attention = jnp.exp(logits - max_logits[..., None])
+    # [batch_size, num_heads, seq_len] -> [batch_size, num_heads]
+    denominator = unnormalized_attention.sum(axis=-1)
+    # [batch_size, num_heads, seq_len] * [batch_size, seq_len, head_dim] -> [batch_size, num_heads, head_dim]
+    attention_output = jnp.einsum("bhl,bld->bhd", unnormalized_attention.astype(xv.dtype), xv) / denominator[..., None]
+
+    return attention_output, max_logits[..., None], denominator[..., None]
+
+
+def reference_multihead_attention(xq, xk, xv, lengths, *, mask_value: float = -float("inf")):
+    """
+    Reference implementation of multihead attention.
+
+    Args:
+        xq: Query jax.Array [batch_size, 1, num_heads, head_dim]
+        xk: Key jax.Array [batch_size, seq_len, num_heads, head_dim]
+        xv: Value jax.Array [batch_size, seq_len, num_heads, head_dim]
+        lengths: jax.Array i32 [batch_size]
+        mask_value: Value used for padding the attention. Default is -inf.
+
+    Returns:
+        attention_output: jax.Array [batch_size, num_heads, head_dim]
+        max_logit: jax.Array [batch_size, num_heads, 1]
+        softmax_denominator: jax.Array [batch_size, num_heads, 1]
+    """
+    # Swap axes for vmap
+    # [batch_size, seq_len, num_heads, head_dim] -> [batch_size, num_heads, seq_len, head_dim]
+    xq = jnp.swapaxes(xq, 1, 2)
+    xk = jnp.swapaxes(xk, 1, 2)
+    xv = jnp.swapaxes(xv, 1, 2)
+
+    # vmap over the num_heads axis, s.t.
+    # in_axes = (1, 1, 1, None) splits the inputs along the num_heads axis
+    # out_axes = 2 combines the outputs along the num_heads axis
+    return jax.vmap(
+        partial(reference_multiquery_attention, mask_value=mask_value),
+        in_axes=(1, 1, 1, None),
+        out_axes=2,
+    )(xq, xk, xv, lengths)
+
+
+def flash_attention_kernel(lengths, q_ref, k_ref, v_ref, o_ref, m_ref, l_ref, *, block_size: int, mask_value: float):
+    """Pallas flash attention kernel."""
+    # get batch index and block index from program id
+    batch, index = pl.program_id(0), pl.program_id(1)
+
+    @pl.when(index == 0)
+    def init():
+        m_ref[...] = jnp.full_like(m_ref, -jnp.inf)
+        l_ref[...] = jnp.zeros_like(l_ref)
+        o_ref[...] = jnp.zeros_like(o_ref)
+
+    length = lengths[batch]
+
+    @pl.when(index * block_size < length)
+    def run():
+        # load data
+        # xq: [batch_size, num_heads, head_dim]
+        # xk: [batch_size, seq_len, head_dim]
+        # xv: [batch_size, seq_len, head_dim]
+        xq = q_ref[...].astype(jnp.float32)
+        xk = k_ref[...].astype(jnp.float32)
+        xv = v_ref[...].astype(jnp.float32)
+
+        # load state
+        m_prev, l_prev = m_ref[...], l_ref[...]
+
+        # Compute scores
+        # qk: [batch_size, num_heads, head_dim] @ [batch_size, seq_len, head_dim] -> [batch_size, num_heads, seq_len]
+        qk = lax.dot_general(xq, xk, (((1,), (1,)), ((), ())), preferred_element_type=jnp.float32)
+        mask = index * block_size + lax.broadcasted_iota(jnp.int32, qk.shape, 1) < length
+        # Create and apply mask: [batch_size, num_heads, seq_len]
+        qk = qk + jnp.where(mask, 0.0, mask_value)
+
+        # Compute max scores
+        m_curr = qk.max(axis=-1)
+
+        # Compute unnormalized attention and accumulated values
+        s_curr = jnp.exp(qk - m_curr[..., None])
+        l_curr = lax.broadcast_in_dim(s_curr.sum(axis=-1), l_prev.shape, (0,))
+        # Compute block output
+        o_curr_dot_l_curr = jnp.dot(s_curr, xv)
+
+        # Update max scores and accumulated values
+        m_curr = lax.broadcast_in_dim(m_curr, m_prev.shape, (0,))
+        m_next = jnp.maximum(m_prev, m_curr)
+        alpha = jnp.exp(m_prev - m_next)
+        beta = jnp.exp(m_curr - m_next)
+        l_next = alpha * l_prev + beta * l_curr
+        # Avoid division by zero
+        l_next_safe = jnp.where(l_next == 0.0, 1.0, l_next)
+
+        # Update max and accumulated values
+        m_ref[...], l_ref[...] = m_next, l_next_safe
+
+        # Update output
+        o_ref[...] = ((l_prev * alpha * o_ref[...] + beta * o_curr_dot_l_curr) / l_next_safe).astype(o_ref.dtype)
+
+
+def ragged_multiquery_attention(
+    xq, xk, xv, lengths, *, block_size: int = 256, mask_value: float = -float("inf"), cost_estimate: float | None = None
+):
+    """Multiquery attention.
+
+    Args:
+        xq: Query jax.Array [batch_size, 1, head_dim]
+        xk: Key jax.Array [batch_size, seq_len, head_dim]
+        xv: Value jax.Array [batch_size, seq_len, head_dim]
+        lengths: jax.Array i32 [batch_size]
+        cost_estimate: A Pallas TPU cost estimate based on the reference implementation.
+
+    Returns:
+        attention_output: jax.Array [batch_size, num_heads, head_dim]
+        max_logit: jax.Array [batch_size, num_heads, 1]
+        softmax_denominator: jax.Array [batch_size, num_heads, 1]
+    """
+    batch_size, num_heads, head_dim = xq.shape
+    assert lengths.shape == (batch_size,)
+    assert lengths.dtype == jnp.int32
+    seq_len = xk.shape[1]
+
+    # Call flash attention kernel with grid (batch_size, num_blocks)
+    # where num_blocks = seq_len // block_size
+    out, m, l = pl.pallas_call(
+        partial(
+            flash_attention_kernel,
+            block_size=block_size,
+            mask_value=mask_value,
+        ),
+        grid=(batch_size, seq_len // block_size),
+        compiler_params={"mosaic": {"dimension_semantics": ("parallel", "arbitrary")}},
+        out_shape=[
+            jax.ShapeDtypeStruct((batch_size, num_heads, head_dim), dtype=jnp.float32),
+            jax.ShapeDtypeStruct((batch_size, num_heads, head_dim), dtype=jnp.float32),
+            jax.ShapeDtypeStruct((batch_size, num_heads, head_dim), dtype=jnp.float32),
+        ],
+        cost_estimate=cost_estimate,
+    )(lengths, xq, xk, xv)
+
+    return out, m, l
+
+
+@partial(jax.jit, static_argnums=["block_size", "mask_value"])
+def ragged_multihead_attention(xq, xk, xv, lengths, *, block_size: int = 256, mask_value: float = -float("inf")):
+    """Ragged multihead attention.
+
+    Args:
+        xq: Query jax.Array [batch_size, 1, num_heads, head_dim]
+        xk: Key jax.Array [batch_size, seq_len, num_heads, head_dim]
+        xv: Value jax.Array [batch_size, seq_len, num_heads, head_dim]
+        lengths: jax.Array i32 [batch_size]
+        block_size: int defining the Pallas block length in the seq_len dimension
+        mask_value: float value for padding
+
+    Returns:
+        attention_output: jax.Array [batch_size, num_heads, head_dim]
+        max_logit: jax.Array [batch_size, num_heads, 1]
+        softmax_denominator: jax.Array [batch_size, num_heads, 1]
+    """
+    shaped_dtypes = [xq, xk, xv, lengths]
+    cost_estimate = get_mha_cost_estimate(shaped_dtypes)
+
+    # Swap axes for vmap
+    query = jnp.swapaxes(xq, 1, 2)
+    key = jnp.swapaxes(xk, 1, 2)
+    value = jnp.swapaxes(xv, 1, 2)
+
+    # vmap over the num_heads axis, and output on the num_heads axis
+    # The paralelization is for each head i, such that:
+    # query[i]: [batch_size, 1, head_dim]
+    # key[i]: [batch_size, seq_len, head_dim]
+    # value[i]: [batch_size, seq_len, head_dim]
+    # lengths[i]: [batch_size] (shared across heads)
+    # out[i]: [batch_size, head_dim]
+    # After vmap, out: [batch_size, num_heads, head_dim]
+    out, m, l = jax.vmap(
+        partial(
+            ragged_multiquery_attention,
+            block_size=block_size,
+            mask_value=mask_value,
+            cost_estimate=cost_estimate,
+        ),
+        in_axes=(1, 1, 1, None),
+        out_axes=2,
+    )(query, key, value, lengths)
+
+    # Add seq_len dimension to m and l
+    # m, l: [batch_size, num_heads] -> [batch_size, 1, num_heads]
+    m = jnp.expand_dims(m, axis=1)
+    l = jnp.expand_dims(l, axis=1)
+
+    return out, m, l
---
+++ test/test_kernels.py
@@ -0,0 +1,87 @@
+"""
+Tests attention kernels, such as MQA, GQA comparing agains the vanilla attention.
+
+Taken from https://github.com/AI-Hypercomputer/maxtext/blob/main/MaxText/tests/kernels_test.py
+
+Minor changes in notation and code style for consistency.
+"""
+
+import unittest
+
+import jax
+import jax.numpy as jnp
+import numpy as np
+
+from src.kernels import (
+    ragged_multiquery_attention,
+    ragged_multihead_attention,
+    reference_multiquery_attention,
+    reference_multihead_attention,
+)
+
+
+class RaggedAttentionTest(unittest.TestCase):
+    """Test ragged attention kernels."""
+
+    batch_size = 4
+    num_kv_heads = 8
+    num_query_heads = 32
+    max_prefill_predict_length = 128
+    max_target_length = 256
+    head_dim = 128
+
+    # The embeddin dimension is then: num_query_heads * head_dim = 32 * 128 = 4096
+
+    dtype = jnp.float32
+    key_0 = jax.random.PRNGKey(0)
+    key_1, key_2, key_3 = jax.random.split(key_0, 3)
+
+    def test_ragged_mqa(self):
+        """Test ragged MQA attention."""
+        xq = jax.random.normal(self.key_1, (self.batch_size, 1, self.head_dim), dtype=self.dtype)
+        xk = jax.random.normal(self.key_2, (self.batch_size, self.max_target_length, self.head_dim), dtype=self.dtype)
+        xv = jax.random.normal(self.key_3, (self.batch_size, self.max_target_length, self.head_dim), dtype=self.dtype)
+        lengths = jnp.array(np.random.randint(1, self.max_target_length, self.batch_size), dtype=jnp.int32)
+
+        ragged_out, ragged_max, _ = ragged_multiquery_attention(xq, xk, xv, lengths)
+        reference_out, reference_max, _ = reference_multiquery_attention(xq, xk, xv, lengths)
+
+        self.assertTrue(
+            jnp.max(abs(ragged_out - reference_out)) < 1e-2,
+            f"Ragged MQA and reference MQA outputs differ: {jnp.max(abs(ragged_out - reference_out))}",
+        )
+
+        self.assertTrue(
+            jnp.average(abs(ragged_max - reference_max)) < 1e-2,
+            f"Ragged MQA and reference MQA average values differ: {jnp.average(abs(ragged_max - reference_max))}",
+        )
+
+    def test_ragged_mha(self):
+        """Test ragged MHA attention."""
+        xq = jax.random.normal(self.key_1, (self.batch_size, 1, self.num_query_heads, self.head_dim), dtype=self.dtype)
+        xk = jax.random.normal(
+            self.key_2, (self.batch_size, self.max_target_length, self.num_query_heads, self.head_dim), dtype=self.dtype
+        )
+        xv = jax.random.normal(
+            self.key_3, (self.batch_size, self.max_target_length, self.num_query_heads, self.head_dim), dtype=self.dtype
+        )
+        lengths = jnp.array(np.random.randint(1, self.max_target_length, self.batch_size), dtype=jnp.int32)
+
+        ragged_out, ragged_max, ragged_denom = ragged_multihead_attention(xq, xk, xv, lengths)
+        ragged_out = ragged_out / ragged_denom
+
+        reference_out, reference_max, _ = reference_multihead_attention(xq, xk, xv, lengths)
+
+        self.assertTrue(
+            jnp.max(abs(ragged_out - reference_out)) < 1e-2,
+            f"Ragged MHA and reference MHA outputs differ: {jnp.max(abs(ragged_out - reference_out))}",
+        )
+
+        self.assertTrue(
+            jnp.average(abs(ragged_max - reference_max)) < 1e-2,
+            f"Ragged MHA and reference MHA average values differ: {jnp.average(abs(ragged_max - reference_max))}",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
