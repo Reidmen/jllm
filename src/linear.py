acb66dd2ac2cd10e95822ccf7cd0901774d5e23a
Reidmen <r.rethmawn@gmail.com>
2025-02-19 14:17:47 +0100

    basic template for more general MLP

--- src/linear.py
+++ src/linear.py
@@ -1,12 +1,14 @@
 """Linear layers. """
 
-from typing import Iterable, Sequence
+from typing import Any, Iterable, Sequence
 
 import jax
 import jax.numpy as jnp
 from flax import linen as nn
 from typing import Callable
 import numpy as np
+
+from model import RMSNorm
 
 class DenseGeneral(nn.Module):
     """Linear Layer with flexible axes. """
@@ -55,3 +57,26 @@ class DenseGeneral(nn.Module):
 
         return output
 
+
+class MLPBlock(nn.Module):
+    """Transformer MLP / Feed-Forward Block"""
+    config: dict[str, Any] 
+    intermediate_dim: int = 2048
+    activations: Sequence[str | Callable[..., Any]] = ("relu",)
+    intermediate_dropout_rate: float = 0.1
+    dtype: jax.dtypes = jnp.float32
+    weight_dtype: jax.dtypes = jnp.float32
+    use_bias: bool = False
+    use_pre_norm: bool = False
+    quantization: str | None = None
+
+    def get_norm_layer(self):
+        if self.config.decoder_block in ("default", "gemma"):
+            return RMSNorm
+        else:
+            raise ValueError(f"Not implemented decoder_block {self.config.decoder_block}")
+        
+    @nn.compact
+    def __call__(self, inputs, decode: bool = False, deterministic: bool = False):
+        """Applies transformer MLPBlock module"""
+        raise NotImplementedError 
