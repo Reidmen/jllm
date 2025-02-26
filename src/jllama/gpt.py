8f0e021afb2f8740f085180e4b2caff048b0594d
Reidmen <r.rethmawn@gmail.com>
2025-02-26 18:12:07 +0100

    more functions and layers

--- src/jllama/gpt.py
+++ src/jllama/gpt.py
@@ -1,26 +1,21 @@
-"""This module contains a simple implementation for GPT"""
+"""This module implement an encoder-only GPT model"""
 
 import jax
 import jax.numpy as jnp
 import flax.linen as nn
 from jllama.utils import ModelArgs
 
-class SelfAttention(nn.Module):
+class Attention(nn.Module):
     """Self-attention mechanism for transformer models.
 
     This module computes the self-attention scores and applies them to the input
     to produce the output representation.
     """
+    args: ModelArgs
+    rate_dropout: float
 
-    def __init__(self, args: ModelArgs, rate_dropout: float) -> None:
-        """Initializes the SelfAttention module.
-
-        Args:
-            args: ModelArgs containing model configuration parameters.
-            rate_dropout: Dropout rate for regularization.
-        """
-        super().__init__()
-        head_size = args.embedding_size // args.num_heads
+    def setup(self) -> None:
+        head_size = self.args.embedding_size // self.args.num_heads
 
         # Linear layers for computing key, query, and value matrices
         self._Wk = nn.Dense(
@@ -40,8 +35,9 @@ class SelfAttention(nn.Module):
         )
 
         # Dropout layer for regularization
-        self.dropout = nn.Dropout(rate_dropout)
+        self.dropout = nn.Dropout(self.rate_dropout)
 
+    @nn.compact
     def forward(self, x: jax.Array) -> jax.Array:
         """Computes the self-attention output.
 
@@ -70,6 +66,63 @@ class SelfAttention(nn.Module):
         return output
 
 
+class MultiHeadAttention(nn.Module):
+    args: ModelArgs
+    rate_dropout: float
+
+    def setup(self) -> None:
+        self.attention_heads = [Attention(self.args, self.rate_dropout) for _ in range(self.args.num_heads)]
+
+    @nn.compact
+    def forward(self, x: jax.Array) -> jax.Array:
+        """Computes the multihead attention output"""
+        outputs = [head(x) for head in self.attention_heads]
+        return jnp.concatenate(outputs, axis=-1) # shape: (B, T, D)
 
 
+class FeedForward(nn.Module):
+    args: ModelArgs
+    rate_dropout: float
+    embedding_factor: int
 
+    @nn.compact
+    def forward(self, x: jax.Array) -> jax.Array:
+        gate_projection = nn.Dense(self.embedding_factor * self.args.embedding_size)(x)
+        activated_output = gate_projection * nn.sigmoid(gate_projection)
+        output_projection = self.Dense(self.args.embedding_size)(activated_output)
+        output = nn.Dropout(self.rate_dropout)(output_projection)
+
+        return output
+
+
+class TransformerBlock(nn.Module):
+    """Transformer Block 
+
+    Input x shape: (B, T, D)
+    Output array shape: (B, T, D)
+    
+    Scheme: 
+    Input (x) --> [LayerNorm] --> [MultiHeadAttention] --> + --> [LayerNorm] --> Output
+                |                                      |
+                +--------------------------------------+
+    """
+    args: ModelArgs
+    rate_dropout: float
+    embedding_factor: int
+
+    def setup(self) -> None:
+        head_size = self.args.embedding_size // self.args.num_heads
+        # Define layers 
+        self.multihead_attention = MultiHeadAttention(self.args, self.rate_dropout)
+        self.feed_forward = FeedForward(self.args, self.rate_dropout, self.embedding_factor)
+        self.input_layer_norm = nn.LayerNorm(self.args.norm_eps)
+        self.output_layer_norm = nn.LayerNorm(self.args.norm_eps)
+
+    @nn.compact
+    def forward(self, x: jax.Array) -> jax.Array:
+        # Input x shape (B, T, D)
+        y = x + self.multihead_attention(self.input_layer_norm(x))
+        # Output multihead attention (B, T, D)
+        output = y + self.output_layer_norm(y)
+
+        return output 
