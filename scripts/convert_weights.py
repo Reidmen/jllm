01c4de45403bb1a250d9e3d60d67bb86e765c081
Reidmen <r.rethmawn@gmail.com>
2025-06-08 17:51:10 -0400

    more uilts

--- scripts/convert_weights.py
+++ scripts/convert_weights.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import argparse
+import shutil
 
 
 def main(model_path: str | Path, chkpt_path: str | Path):
@@ -8,6 +10,7 @@ def main(model_path: str | Path, chkpt_path: str | Path):
   from tqdm import tqdm
 
   model_path, chkpt_path = Path(model_path), Path(chkpt_path)
+  chkpt_path.mkdir(parents=True, exist_ok=True)
   files = list(model_path.glob("**/*safetensors"))
   if len(files) < 2:
     raise FileNotFoundError  # config and safetensors must exist
@@ -29,6 +32,30 @@ def main(model_path: str | Path, chkpt_path: str | Path):
     # compatible_weights = utils.convert_model_or_layer(mlp_layer, model, cfg, sequential=False)
     # save_pytree(compatible_weights, chk_path)
 
+  additional_files = ["config.json", "tokenizer.json", "tokenizer_config.json"]
+  for additional_file in additional_files:
+    full_paths = list(model_path.glob(f"**/{additional_file}"))
+    if len(full_paths) != 1:
+      print(f"Found more than one file for {additional_file}")
+    if len(full_paths) == 0:
+      continue
+    full_path = full_paths[0]
+    shutil.copyfile(full_path, chkpt_path / full_path.name)
+
 
 if __name__ == "__main__":
-  main("./hf_models/Qwen--Qwen3-0.6B", "./jax_models/Qwen--Qwen3-0.6B")
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+    "--hf_model_path",
+    required=False,
+    default="./hf_models/Qwen--Qwen3-0.6B",
+    help="HuggingFace model path e.g. ./hf_models/Qwen--Qwen3-0.6B"
+  )
+  parser.add_argument(
+    "--jax_model_path",
+    required=False,
+    default="./jax_models/Qwen--Qwen3-0.6B",
+    help="Destination path for jax-compatible model"
+  )
+  args = parser.parse_args()
+  main(args.hf_model_path, args.jax_model_path)
