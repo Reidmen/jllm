d90c4b758ab844c89bb2824e987cb47238d043ca
Reidmen <r.rethmawn@gmail.com>
2025-06-08 23:44:24 -0400

    updates on utils

--- src/jllama/qwen_utils.py
+++ src/jllama/qwen_utils.py
@@ -5,6 +5,7 @@ Reference:
 """
 
 import os
+import re
 import jax
 import jax.numpy as jnp
 
@@ -35,6 +36,28 @@ def _index_to_str(x):
       return str(getattr(x, field))
   raise ValueError
 
+def convert_weight(key: str, value: torch.Tensor, cfg: Config):
+  """Preserves HF checkpoint naming convention"""
+  value = value.detach()
+  # Attention 
+  if re.search(r"q_proj\.weight", key) is not None:
+    assert value.shape == (cfg.embed_size * cfg.head_dim, cfg.head_dim)
+    return torch_to_jax(value.T.reshape((cfg.embed_size, cfg.q_heads, cfg.head_dim)))
+  elif re.search(r"[kv]_proj\.weight", key) is not None:
+    assert value.shape == (cfg.kv_heads * cfg.head_dim, cfg.embed_size)
+    return torch_to_jax(value.T.reshape((cfg.embed_size, cfg.kv_heads, cfg.head_dim)))
+  elif re.search(r"o_proj\.weight", key) is not None:
+    assert value.shape == (cfg.embed_size, cfg.q_heads * cfg.head_dim)
+    return torch_to_jax(value.T.reshape((cfg.q_heads, cfg.head_dim, cfg.embed_size)))
+  # MLP
+  elif re.search(r"down_proj", key) is not None:
+    assert value.shape == (cfg.embed_size, cfg.mlp_ffw_size)
+    return torch_to_jax(value.T)
+  elif re.search(r"(gate|up)_proj", key) is not None:
+    assert value.shape == (cfg.mlp_ffw_size, cfg.embed_size)
+    return torch_to_jax(value.T)
+  else:
+    raise ValueError(f"Unknown weight {key}")  
 
 def convert_model_weights(
   layer: MLPLayer, reference_layer: torch.nn.Module, cfg: Config, device: jax.Device | None = None
@@ -48,4 +71,33 @@ def convert_model_weights(
     ".".join(map(_index_to_str, k)): v for (k, v) in jax.tree.flatten_with_path(layer, is_leaf=is_leaf)[0]
   }
   new_params = {k: None for k in layer_params.keys()}
-  pass
+
+  def convert_weight_per_thread(tkey, tweight):
+    with jax.default_device(device):
+      jweight = convert_weight(tkey, _map_weight(tkey, tweight), cfg)
+    jkey = _torch_to_jax_key(tkey)
+    if jkey is None:
+      raise ValueError(f"Could not find parameter mapping for torch parameter: {tkey}")
+    if jkey is not in new_params:
+      raise ValueError(f"The JAX model is not expecting {jkey}")
+    if new_params[jkey] is not None:
+      raise ValueError(f"Parameter {jkey} already exist!")
+    new_params[jkey] = jweight
+
+  # TODO: multithread version 
+  for tkey, tweight in torch_params.items():
+    convert_weight_per_thread(tkey, tweight)
+  
+  if not all(v is not None for v in new_params.values()):
+    raise ValueError(str({k: v for k, v in new_params.items() if v is not None}))
+
+  if isinstance(layer, MLPLayer):
+    return jax.tree.unflatten(jax.tree.structure(layer, is_leaf=is_leaf), new_params.values())
+  else:
+    return jax.tree.unflatten(
+      jax.tree.structure(layer, is_leaf=is_leaf),
+      [
+        new_param if new_param is None else param
+        for (new_param, param) in zip(new_params.values(), layer_params.values())
+      ]
+    )
