8c4d1775fab231f6e6a41cad4efff7721144fad5
Reidmen <r.rethmawn@gmail.com>
2025-08-26 00:08:03 -0400

    adding basic test

--- src/jllm/qwen/flash.py
+++ src/jllm/qwen/flash.py
@@ -3,9 +3,10 @@ import jax
 import jax.numpy as jnp
 from jax.sharding import PartitionSpec
 from jax.experimental.shard_map import shard_map
+from jax.sharding import NamedSharding 
 from jax.experimental.pallas.ops.tpu.splash_attention import splash_attention_kernel, splash_attention_mask
 import jax.experimental.pallas as pl
-from jax.experimental.pallas import tpu as tpu
+from jax.experimental.pallas import tpu as pltpu
 from functools import partial
 
 NUM_LANES = 128
@@ -19,8 +20,10 @@ def logical_to_physical(logical, rules) -> PartitionSpec:
     raise ValueError("Duplicate axes in sharding rules")
   return PartitionSpec(*spec)
 
+
 def attn_device_precision():
   return jax.devices()[0].device_kind.lower() in ["tpu v2", "tpu v3"]
+
 
 def attention_kernel(
   q: jax.Array,  # (batch_size, qnum_heads, qseq_len, head_dim)
@@ -33,8 +36,8 @@ def attention_kernel(
   lengths: jax.Array,
   cfg,
 ):
-  if attn_device_precision(): # TPU v2/v3 need f32
-    q, k, v = q.astype(jnp.float32), k.astype(jnp.float32), v.astype(jnp.float32) 
+  if attn_device_precision():  # TPU v2/v3 need f32
+    q, k, v = q.astype(jnp.float32), k.astype(jnp.float32), v.astype(jnp.float32)
   """Flash (GQ)-Attention kernel."""
   if q.shape[-3] % k.shape[-3] != 0:  # Required for GQA
     raise Exception
@@ -64,9 +67,9 @@ def attention_kernel(
     # k, v: (batch_size, kv_heads, seq_len, head_dim)
     if q.shape[-2] == 1:
       # print(f"Decode kernel, {q.shape=}, seq_len {q.shape[-1]}")
-      in_axis = (1, 1, 1, None, None)  # for block_kv, block_bs?
+      in_axis = (1, 1, 1, None, None)
       params = dict(scale=scale, block_kv=128, block_bs=32, interpret=False)
-      q = q[..., 0, :]
+      q = q[..., 0, :]  # (batch_size, kv_heads, q_heads, head_dim)
       attn_ret = jax.vmap(partial(ragged_attention_fwd, **params), in_axes=in_axis, out_axes=1)(
         q, k, v, starts, lengths
       )
@@ -84,6 +87,7 @@ def attention_kernel(
       segment_ids = splash_attention_kernel.SegmentIds(q=q_segment_ids, kv=kv_segment_ids)
       attn_ret = attn_fn(q * scale, k, v, segment_ids)
 
+    assert isinstance(attn_ret, jax.Array)
     return attn_ret.reshape(q.shape)
 
   lengths = jnp.broadcast_to(lengths, starts.shape)
@@ -119,7 +123,7 @@ def _flash_attention_fwd(
       assert axis in (-1, x.ndim - 1)
       return x[..., :new_size_in_dim]
 
-    return tpu.repeat(x, new_size_in_dim // x.shape[axis], axis=axis % x.ndim)
+    return pltpu.repeat(x, new_size_in_dim // x.shape[axis], axis=axis % x.ndim)
 
   @pl.when(i == 0)
   def init():
@@ -173,15 +177,15 @@ def _flash_attention_fwd(
 
 
 def ragged_attention_fwd(
-  q: jax.Array,
-  k: jax.Array,
-  v: jax.Array,
+  q: jax.Array,  # (batch_size, q_heads, head_dim)
+  k: jax.Array,  # (batch_size, kv_seq_len, head_dim)
+  v: jax.Array,  # (batch_size, kv_seq_len, head_dim)
   starts: jax.Array | None = None,
   lengths: jax.Array | None = None,
-  qk_prev: jax.Array | None = None,
+  qk_prev: jax.Array | None = None,  # (batch_size, q_heads, kv_seq_len)
   block_bs: int = 4,
   block_kv: int = 256,
-  scale: int | None = None,
+  scale: float | None = None,
   interpret: bool = False,
 ):
   scale = math.sqrt(q.shape[-1]) if scale is None else scale
@@ -236,15 +240,15 @@ def ragged_attention_fwd(
     in_specs += [None]
 
   out_shape = jax.ShapeDtypeStruct((bs, q_heads, head_dim_v), q.dtype)
-  grid_spec = tpu.PrefetchScalarGridSpec(
+  grid_spec = pltpu.PrefetchScalarGridSpec(
     num_scalar_prefetch=4,
     grid=(bs // block_bs, kv_seq_len // block_kv),
     in_specs=in_specs,
     out_specs=pl.BlockSpec((block_bs, q_heads, head_dim_v), lambda b, i, *_: (b, 0, 0)),
     scratch_shapes=[
-      tpu.VMEM((block_bs, q_heads, head_dim_v), dtype=jnp.float32),
-      tpu.VMEM((block_bs, q_heads, NUM_LANES), dtype=jnp.float32),
-      tpu.VMEM((block_bs, q_heads, NUM_LANES), dtype=jnp.float32),
+      pltpu.VMEM((block_bs, q_heads, head_dim_v), dtype=jnp.float32),
+      pltpu.VMEM((block_bs, q_heads, NUM_LANES), dtype=jnp.float32),
+      pltpu.VMEM((block_bs, q_heads, NUM_LANES), dtype=jnp.float32),
     ],
   )
   params = dict(kv_seq_len=kv_seq_len, block_kv=block_kv, block_bs=block_bs, scale=scale)
@@ -255,11 +259,111 @@ def ragged_attention_fwd(
   return attn
 
 
+def ragged_attention_fwd_reference(
+  q: jax.Array,  # (batch_size, q_heads, head_dim)
+  k: jax.Array,  # (batch_size, kv_seq_len, head_dim)
+  v: jax.Array,  # (batch_size, kv_seq_len, head_dim)
+  starts: jax.Array | None = None,
+  lenghts: jax.Array | None = None,
+  qk_prev: jax.Array | None = None,  # (batch_size, q_heads, kv_seq_len)
+  block_q: int = 16,
+  block_kv: int = 256,
+  scale: float | None = None,
+):
+  scale = math.sqrt(q.shape[-1]) if scale is None else scale
+  bs, q_heads, _ = q.shape
+  bs_k, kv_seq_len, _ = k.shape
+  bs_v, kv_seq_len_v, _ = v.shape
+  if starts is None:
+    starts = jnp.zeros((bs,), dtype=jnp.int32)
+  if lenghts is None:
+    lenghts = kv_seq_len * jnp.ones((bs,), dtype=jnp.int32)
+  qk = jnp.einsum("bqh,bth->bqt", q, k)  # (batch_size, q_heads, kv_seq_len)
+  if qk_prev is not None:
+    qk = qk + qk_prev
+  qk = qk * scale
+  indices = jnp.arange(kv_seq_len)  # [0, 1, ... kv_seq_len]
+  mask = (indices >= starts[:, None]) & (indices < lenghts[:, None])
+  qk = jnp.where(mask[:, None, :], qk, jnp.finfo(qk.dtype).min)
+  sm = jax.nn.softmax(qk, axis=-1) * (jnp.sum(mask, -1) > 0)[:, None, None]
+  return jnp.einsum("bqt,bth->bqh", sm, v)
+
+
 def test_ragged_attention(interpret: bool = False):
-  "https://github.com/jax-ml/jax-llm-examples/blob/main/qwen3/qwen3_jax/ragged_attention.py"
-  raise NotImplementedError
   bs, q_heads, kv_heads, kv_seq_len, head_dim = 64, 8, 4, 1024, 128
   print("Testing ragged flash attention")
   dtype = jnp.bfloat16
-  mesh = jax.make_mesh((jax.devices(),), ("x"))
+  mesh = jax.make_mesh((jax.device_count(),), axis_names=("x",), devices=jax.devices())
+
+  @partial(jax.jit, static_argnames=("which", "block_kv", "block_bs"))
+  def attention_test(
+    q: jax.Array,
+    k: jax.Array,
+    v: jax.Array,
+    starts: jax.Array,
+    lengths: jax.Array,
+    qk_prev: jax.Array | None = None,
+    which: str = "pallas",
+    block_kv: int = 124,
+    block_bs: int = 8,
+  ):
+    kv_heads = k.shape[1]
+    q_grouped = q.reshape(q.shape[:1] + (kv_heads, -1) + q.shape[2:])
+    if qk_prev is not None:
+      qk_prev_grouped = qk_prev.reshape(qk_prev[:1] + (kv_heads, -1) + qk_prev[2:])
+
+    qkv_spec = PartitionSpec(None, "x", None, None)
+    in_specs = 3 * (qkv_spec,) + 2 * (PartitionSpec(),)
+    in_specs += (PartitionSpec(None, "x", None, None) if qk_prev is not None else None,)
+    out_specs = qkv_spec
+
+    @partial(shard_map, mesh=mesh, in_specs=in_specs, out_specs=out_specs)
+    def _fn(q, k, v, starts, lenghts, qk_prev):
+      in_axes = (1, 1, 1, None, None)
+      in_axes += (1 if qk_prev is not None else None,)
+      if which == "pallas":
+        opts = dict(block_kv=block_kv, block_bs=block_bs)
+        return jax.vmap(
+          partial(ragged_attention_fwd, **opts), in_axes=in_axes, out_axes=1
+        )(q, k, v, starts, lenghts, qk_prev)
+      elif which == "naive":
+        return jax.vmap(
+          ragged_attention_fwd_reference, in_axes=in_axes, out_axes=1
+        )(q, k, v, starts, lenghts, qk_prev)
+    
+    return _fn(q_grouped, k, v, starts, lengths, qk_prev_grouped).reshape(q.shape)
+  
+  keys = jax.random.split(jax.random.key(0), 1024)
+  keyiter = iter(keys)
+  q = jax.random.normal(next(keyiter), (bs, q_heads, head_dim), dtype=dtype)
+  k = jax.random.normal(next(keyiter), (bs, kv_heads, kv_seq_len, head_dim), dtype=dtype) 
+  v = jax.random.normal(next(keyiter), (bs, kv_heads, kv_seq_len, head_dim), dtype=dtype) 
+  # Normalize and put on device
+  q = jax.device_put(q / jnp.linalg.norm(q, axis=-1)[..., None], NamedSharding(mesh, PartitionSpec(None, "x", None)))
+  k = jax.device_put(k / jnp.linalg.norm(k, axis=-1)[..., None], NamedSharding(mesh, PartitionSpec(None, "x", None, None)))
+  v = jax.device_put(v / jnp.linalg.norm(v, axis=-1)[..., None], NamedSharding(mesh, PartitionSpec(None, "x", None, None)))
+  qk_prev = None
+
+  starts = jnp.zeros((bs,), dtype=jnp.int32)
+  sparsity_factor = 8
+  min_val, max_val = -kv_seq_len / sparsity_factor, kv_seq_len / sparsity_factor
+  base_length_position = round(kv_seq_len / sparsity_factor) * jnp.ones((bs,), dtype=jnp.int32)
+  random_length_positions = jax.random.randint(next(keyiter), (bs,), min_val, max_val, dtype=jnp.int32)
+  lengths = base_length_position + random_length_positions
+  print(f"Sparse Cache {lengths=}")
+  replicated_sharding = NamedSharding(mesh, PartitionSpec())
+  starts, lengths = jax.device_put(starts, replicated_sharding), jax.device_put(lengths, replicated_sharding)
+  assert isinstance(q, jax.Array) and isinstance(k, jax.Array) and isinstance(v, jax.Array)
+  total_mem = sum([
+    elem.size * elem.itemsize / sparsity_factor for elem in [q, k, v]
+  ]) / jax.device_count()
+  print(f"Sparse total memory {total_mem:.5e}")
+  print(f"Sparse HBM speed {1e6 * total_mem / 819e9:.5e} us")
+
+  attention_pallas = attention_test(q, k, v, starts, lengths, qk_prev, which="pallas")
+  attention_reference = attention_test(q, k, v, starts, lengths, qk_prev, which="naive") 
+  error = jnp.linalg.norm((attention_pallas - attention_reference).astype(jnp.float32), axis=-1)
+  error = error / jnp.linalg.norm(attention_reference.astype(jnp.float32), axis=-1)
+  print(f"Average error: {error}")
+
 
